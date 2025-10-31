427736c6bbc023ea8771dfa96bc77bdc6e17c94a
Goldbricker32 <maxwell.marson@tcu2905.us>
2025-10-31 14:39:16 -0500

    Delete script.js
    Signed-off-by: Goldbricker32 <maxwell.marson@tcu2905.us>

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const games = document.querySelectorAll('.game-list a');
-    const randomGame = games[Math.floor(Math.random() * games.length)];
-    console.log(`Random game suggestion: ${randomGame.textContent}`);
-});
-
